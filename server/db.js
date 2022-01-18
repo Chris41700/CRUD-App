fe647ff36333f0b8663c41a6ca8356b3e64344d7
Chris41700 <ChrisHui417@gmail.com>
2022-01-18 14:59:57 -0500

    Build Routes with PostgreSQL Quries

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "kth18822",
+    password: "TheLazyCoder41700",
     host: "localhost",
     port: 5432,
     database: "perntodo"
