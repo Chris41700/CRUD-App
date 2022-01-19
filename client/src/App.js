329d40914a2b5335b7e35098ccf780ec94fc4801
Chris41700 <ChrisHui417@gmail.com>
2022-01-19 15:01:42 -0500

    Setting up ListTodo

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React, { Fragment } from "react";
 
 //Components
 import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodo";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   );
