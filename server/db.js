6d9c2b6c91cff0c91301823bab3e9d6c49f5af5c
Chris41700 <ChrisHui417@gmail.com>
2022-01-20 18:30:58 -0500

    secret

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
     user: "postgres",
-    password: "TheLazyCoder41700",
+    password: secret,
     host: "localhost",
     port: 5432,
     database: "perntodo"
